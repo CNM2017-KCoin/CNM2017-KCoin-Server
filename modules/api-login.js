fc7caf5fa8cabcc3c9db47a820c04f7fd0b0ae24
Tiên Phạm Đức <phamductien133@gmail.com>
2018-01-02 00:55:49 +0700

    Add login

---
+++ modules/api-login.js
@@ -0,0 +1,48 @@
+let dbHelper = require('../helpers/db-helper');
+
+exports.login = function (req, res) {
+  let params = req.body || {};
+  let email = params['email'] || '';
+  let password = params['password'] || '';
+  dbHelper.dbLoadSql(
+    `SELECT id 
+    FROM tb_login l
+    WHERE l.email = ?
+    AND l.password = ?`,
+    [
+      email,
+      password
+    ]
+  ).then(
+    function (userInfo) {
+      console.log(userInfo);
+      let data = [];
+      if (userInfo[0]['id'] > 0) {
+        data = {
+          'status': '200',
+          'data': {
+            'report': 'Đăng nhập thành công!'
+          }
+        };
+      } else {
+        data = {
+          'status': '500',
+          'data': {
+            'error': 'Đăng nhập thất bại!'
+          }
+        };
+      }
+      res.send(data);
+    }
+  ).catch(function (error) {
+      let data = {
+        'status': '500',
+        'data': {
+          'error': error
+        }
+      };
+      res.send(data);
+    }
+  );
+};
+
--- routes/k-coin-api.js
+++ routes/k-coin-api.js
@@ -3,6 +3,7 @@ let coreApiRoute = express.Router();
 
 let apiTest = require('../modules/api-test');
 let apiRegister = require('../modules/api-register');
+let apiLogin = require('../modules/api-login');
 
 coreApiRoute.get('/test', function(req, res) {
   apiTest.test(req, res);
@@ -13,4 +14,8 @@ coreApiRoute.post('/register', function(req, res) {
   apiRegister.register(req, res);
 });
 
+// Api login
+coreApiRoute.post('/login', function(req, res) {
+  apiLogin.login(req, res);
+});
 module.exports = coreApiRoute;
