2f10ede18cdb64175f7026d3d2248a3f245224d7
phamtien133 <phamductien133@gmail.com>
2018-01-02 18:22:05 +0700

    add api transaction
    add api transaction

---
+++ modules/api-transaction.js
@@ -0,0 +1,48 @@
+let dbHelper = require('../helpers/db-helper');
+
+exports.saveTransaction = function (req, res) {
+  let params = req.body || {};
+  let email = params['email'] || '';
+  let password = params['password'] || '';
+  dbHelper.dbLoadSql(
+    `SELECT id 
+    FROM tb_login l
+    WHERE l.email = ?
+    AND l.password = ?`,
+    [
+      email,
+      password
+    ]
+  ).then(
+    function (userInfo) {
+      console.log(userInfo);
+      let data = [];
+      if (userInfo[0]['id'] > 0) {
+        data = {
+          'status': '200',
+          'data': {
+            'report': 'Đăng nhập thành công!'
+          }
+        };
+      } else {
+        data = {
+          'status': '500',
+          'data': {
+            'error': 'Đăng nhập thất bại!'
+          }
+        };
+      }
+      res.send(data);
+    }
+  ).catch(function (error) {
+      let data = {
+        'status': '500',
+        'data': {
+          'error': 'Đã có lỗi xảy ra... Vui lòng thử lại!'
+        }
+      };
+      res.send(data);
+    }
+  );
+};
+
