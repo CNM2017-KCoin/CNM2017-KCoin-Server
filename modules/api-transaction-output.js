99240b445061a6bb08e8e28014b04422ffa55ab1
phamtien133 <phamductien133@gmail.com>
2018-01-08 22:03:33 +0700

    update apt output - with one select
    update apt output - with one select

--- modules/api-transaction-output.js
+++ modules/api-transaction-output.js
@@ -4,8 +4,9 @@ exports.getOutputData = function (req, res) {
   let params = req.body || {};
   let email = params['email'] || '';
   let offset = params['offset'] || '';
+  let sender_data = [];
   dbHelper.dbLoadSql(
-    `SELECT id, role 
+    `SELECT l.id, l.role 
     FROM tb_login l
     WHERE l.email = ?`,
     [
@@ -27,9 +28,9 @@ exports.getOutputData = function (req, res) {
             if (TotalSend[0]['total_send'] == 0) {
               let data = {
                 'status': 500,
-                'error': 'Không tồn tại dữ liệu ',
+                'error': 'Không tồn tại dữ liệu!',
                 'data': {
-                  'total_sender_trans': TotalSend[0]['total_send'],
+                  'total_sender_trans': 0,
                   'sender_trans': {},
                   'limit': 10,
                   'offset': offset
@@ -38,7 +39,7 @@ exports.getOutputData = function (req, res) {
               res.send(data);
             }
             dbHelper.dbLoadSql(
-              `SELECT t.id
+              `SELECT t.id, t.created_at, t.send_amount, t.status
               FROM tb_transaction t
               LEFT JOIN tb_transaction_input ti ON t.id = ti.transaction_id
               WHERE ti.user_id = ?
@@ -65,51 +66,41 @@ exports.getOutputData = function (req, res) {
                   res.send(data);
                 }
                 //, t.created_at, t.send_amount, t.status
-                let sender_data = [];
+                let test = 0;
                 for (let i = 0; i < transactionIdList.length; i++) {
+                  console.log('id: '+transactionIdList[i].id);
                   dbHelper.dbLoadSql(
-                    `SELECT t.created_at, t.send_amount, t.status
-                    FROM tb_transaction t
-                    WHERE t.id = ?`,
+                    `SELECT tto.user_id, tto.address
+                    FROM tb_transaction_output tto
+                    WHERE tto.transaction_id = ?`,
                     [
                       transactionIdList[i]['id']
                     ]
                   ).then(
-                    function (sendInfo) {
-                      for (let j = 0; j < sendInfo.length; j++) {
-                        dbHelper.dbLoadSql(
-                          `SELECT tto.user_id, tto.address
-                          FROM tb_transaction_output tto
-                          WHERE tto.transaction_id = ?`,
-                          [
-                            transactionIdList[i]['id']
-                          ]
-                        ).then(
-                          function (outputInfo) {
-                            let temp = {
-                              'transaction_id': transactionIdList[i]['id'],
-                              'timestamp': sendInfo[j]['created_at'],
-                              'amount': sendInfo[j]['send_amount'],
-                              'status': sendInfo[j]['status'],
-                              'receiver_id': outputInfo[0]['user_id'],
-                              'receiver_address': outputInfo[0]['address'],
-                            };
-                            sender_data.push(temp);
-                            if (j == sendInfo.length - 1 && i == transactionIdList.length - 1) {
-                              let data = {
-                                'status': 200,
-                                'report': 'Lấy dữ liệu thành công!',
-                                'data': {
-                                  'total_sender_trans': TotalSend[0]['total_send'],
-                                  'sender_trans': sender_data,
-                                  'limit': 10,
-                                  'offset': offset
-                                }
-                              };
-                              res.send(data);
-                            }
+                    function (outputInfo) {
+                      let temp = {
+                        'transaction_id': transactionIdList[i]['id'],
+                        'timestamp': transactionIdList[0]['created_at'],
+                        'amount': transactionIdList[0]['send_amount'],
+                        'status': transactionIdList[0]['status'],
+                        'receiver_id': outputInfo[0]['user_id'],
+                        'receiver_address': outputInfo[0]['address'],
+                      };
+                      sender_data.push(temp);
+                      console.log('11111111'+ sender_data.length);
+                      console.log('22222222'+ TotalSend[0]['total_send']);
+                      if ((sender_data.length == TotalSend[0]['total_send'])) {
+                        let data = {
+                          'status': 200,
+                          'report': 'Lấy dữ liệu thành công!',
+                          'data': {
+                            'total_sender_trans': TotalSend[0]['total_send'],
+                            'sender_trans': sender_data,
+                            'limit': 10,
+                            'offset': offset
                           }
-                        );
+                        };
+                        res.send(data);
                       }
                     }
                   );
