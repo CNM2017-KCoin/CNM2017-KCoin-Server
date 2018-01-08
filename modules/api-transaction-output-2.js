78ad9f6fe0f59739b99c16145dc41c8765ae4ca9
phamtien133 <phamductien133@gmail.com>
2018-01-08 20:39:32 +0700

    update trans output
    update trans output

---
+++ modules/api-transaction-output-2.js
@@ -0,0 +1,134 @@
+let dbHelper = require('../helpers/db-helper');
+
+exports.getOutputData = function (req, res) {
+  let params = req.body || {};
+  let email = params['email'] || '';
+  let offset = params['offset'] || '';
+  dbHelper.dbLoadSql(
+    `SELECT id, role 
+    FROM tb_login l
+    WHERE l.email = ?`,
+    [
+      email
+    ]
+  ).then(
+    function (userInfo) {
+      if (userInfo[0]['id'] > 0) {
+        dbHelper.dbLoadSql(
+          `SELECT COUNT(t.id) as total_send
+          FROM tb_transaction t
+          LEFT JOIN tb_transaction_input ti ON t.id = ti.transaction_id
+          WHERE ti.user_id = ?`,
+          [
+            userInfo[0]['id']
+          ]
+        ).then(
+          function (TotalSend) {
+            if (TotalSend[0]['total_send'] == 0) {
+              let data = {
+                'status': 500,
+                'error': 'Không tồn tại dữ liệu ',
+                'data': {
+                  'total_sender_trans': TotalSend[0]['total_send'],
+                  'sender_trans': {},
+                  'limit': 10,
+                  'offset': offset
+                }
+              };
+              res.send(data);
+            }
+            dbHelper.dbLoadSql(
+              `SELECT t.id
+              FROM tb_transaction t
+              LEFT JOIN tb_transaction_input ti ON t.id = ti.transaction_id
+              WHERE ti.user_id = ?
+              LIMIT ?
+              OFFSET ?`,
+              [
+                userInfo[0]['id'],
+                10,
+                offset * 10
+              ]
+            ).then(
+              function (transactionIdList) {
+                if (transactionIdList.length == 0) {
+                  let data = {
+                    'status': 500,
+                    'error': 'Không tồn tại dữ liệu phân trang này',
+                    'data': {
+                      'total_sender_trans': TotalSend[0]['total_send'],
+                      'sender_trans': {},
+                      'limit': 10,
+                      'offset': offset
+                    }
+                  };
+                  res.send(data);
+                }
+                //, t.created_at, t.send_amount, t.status
+                let sender_data = [];
+                for (let i = 0; i < transactionIdList.length; i++) {
+                  dbHelper.dbLoadSql(
+                    `SELECT t.created_at, t.send_amount, t.status
+                    FROM tb_transaction t
+                    WHERE t.id = ?`,
+                    [
+                      transactionIdList[i]['id']
+                    ]
+                  ).then(
+                    function (sendInfo) {
+                      for (let j = 0; j < sendInfo.length; j++) {
+                        dbHelper.dbLoadSql(
+                          `SELECT tto.user_id, tto.address
+                          FROM tb_transaction_output tto
+                          WHERE tto.transaction_id = ?`,
+                          [
+                            sendInfo[j]['id']
+                          ]
+                        ).then(
+                          function (outputInfo) {
+                            let temp = {
+                              'transaction_id': transactionIdList[i]['id'],
+                              'timestamp': sendInfo[j]['created_at'],
+                              'amount': sendInfo[j]['send_amount'],
+                              'status': sendInfo[j]['status'],
+                              'receiver_id': outputInfo['id'],
+                              'receiver_address': outputInfo['address'],
+                            };
+                            sender_data.push(temp);
+                            if (j == sendInfo.length - 1 && i == transactionIdList.length - 1) {
+                              let data = {
+                                'status': 200,
+                                'report': 'Lấy dữ liệu thành công!',
+                                'data': {
+                                  'total_sender_trans': TotalSend[0]['total_send'],
+                                  'sender_trans': sender_data,
+                                  'limit': 10,
+                                  'offset': offset
+                                }
+                              };
+                              res.send(data);
+                            }
+                          }
+                        );
+                      }
+                    }
+                  );
+                }
+              }
+            );
+          }
+        );
+      }
+    }
+  ).catch(function (error) {
+      let data = {
+        'status': '500',
+        'data': {
+          'error': 'Đã có lỗi xảy ra... Vui lòng thử lại!'
+        }
+      };
+      res.send(data);
+    }
+  );
+};
+
