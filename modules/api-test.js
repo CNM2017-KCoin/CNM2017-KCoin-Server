f3040b189db68ef499c639addaefbfef08b08e06
phamtien133 <phamductien133@gmail.com>
2018-01-09 02:54:24 +0700

    update api test with 2fa
    update api test with 2fa

--- modules/api-test.js
+++ modules/api-test.js
@@ -1,7 +1,43 @@
 let dbHelper = require('../helpers/db-helper');
 let nodemailer = require('nodemailer');
+let speakeasy = require('speakeasy');
+
 exports.test = function (req, res) {
-  var transporter = nodemailer.createTransport({
+  var secret = speakeasy.generateSecret({length: 20});
+  // Returns an object with secret.ascii, secret.hex, and secret.base32.
+  // Also returns secret.otpauth_url, which we'll use later.
+  console.log(secret);
+  var token = speakeasy.totp({
+    secret: secret.base32,
+    encoding: 'base32'
+  });
+
+  console.log(token);
+  // Verify a given token
+  var tokenValidates = speakeasy.totp.verify({
+    secret: secret.base32,
+    encoding: 'base32',
+    token: token,
+    window: 6
+  });
+  // Returns true if the token matches
+  if (tokenValidates == true) {
+    console.log('ok');
+  }
+
+  // Verify a given token is within 3 time-steps (+/- 2 minutes) from the server
+  // time-step.
+    var tokenDelta = speakeasy.totp.verifyDelta({
+      secret: secret.base32,
+      encoding: 'base32',
+      token: token,
+      window: 2,
+      step: 60
+    });
+  console.log('hmmm' + tokenDelta);
+  // Returns {delta: 0} where the delta is the time step difference
+  // between the given token and the current time
+  /*var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: 'phamductien133@gmail.com',
@@ -22,5 +58,5 @@ exports.test = function (req, res) {
     } else {
       console.log('Email sent: ' + info.response);
     }
-  });
+  });*/
 };
