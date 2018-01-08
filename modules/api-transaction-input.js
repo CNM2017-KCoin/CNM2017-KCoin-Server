e7f04d3ee3fac3e4ece341a091bc8fd0d5fd9d84
phamtien133 <phamductien133@gmail.com>
2018-01-08 22:12:41 +0700

    update api transaction input - change to one select
    update api transaction input - change to one select

--- modules/api-transaction-input.js
+++ modules/api-transaction-input.js
@@ -40,16 +40,18 @@ exports.getInputData = function (req, res) {
               res.send(data);
             }
             dbHelper.dbLoadSql(
-              `SELECT t.id
+              `SELECT t.id, t.created_at, t.send_amount, t.status
               FROM tb_transaction t
               LEFT JOIN tb_transaction_output tto ON t.id = tto.transaction_id
               WHERE tto.user_id = ?
+              AND t.status = ?
               LIMIT ?
               OFFSET ?`,
               [
                 userInfo[0]['id'],
+                'success',
                 10,
-                offset*10
+                offset * 10
               ]
             ).then(
               function (transactionIdList) {
@@ -70,50 +72,37 @@ exports.getInputData = function (req, res) {
                 let receiver_data = [];
                 for (let i = 0; i < transactionIdList.length; i++) {
                   dbHelper.dbLoadSql(
-                    `SELECT t.created_at, t.send_amount, t.status
-                    FROM tb_transaction t
-                    WHERE t.id = ?`,
+                    `SELECT ti.user_id, ti.address, ti.ref_hash, ti.ref_index
+                    FROM tb_transaction_input ti
+                    WHERE ti.transaction_id = ?`,
                     [
                       transactionIdList[i]['id']
                     ]
                   ).then(
-                    function (ReceiveInfo) {
-                      for (let j = 0; j < ReceiveInfo.length; j++) {
-                        dbHelper.dbLoadSql(
-                          `SELECT ti.user_id, ti.address, ti.ref_hash, ti.ref_index
-                          FROM tb_transaction_input ti
-                          WHERE ti.transaction_id = ?`,
-                          [
-                            transactionIdList[i]['id']
-                          ]
-                        ).then(
-                          function (inputInfo) {
-                            let temp = {
-                              'transaction_id': transactionIdList[i]['id'],
-                              'timestamp': ReceiveInfo[j]['created_at'],
-                              'amount': ReceiveInfo[j]['send_amount'],
-                              'status': ReceiveInfo[j]['status'],
-                              'sender_id': inputInfo[0]['user_id'],
-                              'sender_address': inputInfo[0]['address'],
-                              'ref_hash': inputInfo[0]['ref_hash'],
-                              'ref_index': inputInfo[0]['ref_index'],
-                            };
-                            receiver_data.push(temp);
-                            if (i == transactionIdList.length - 1 && j == ReceiveInfo.length - 1) {
-                              let data = {
-                                'status': 200,
-                                'report': 'Lấy dữ liệu thành công!',
-                                'data': {
-                                  'total_receiver_trans': TotalReceive[0]['total_receive'],
-                                  'receiver_trans': receiver_data,
-                                  'limit': 10,
-                                  'offset': offset
-                                }
-                              };
-                              res.send(data);
-                            }
+                    function (inputInfo) {
+                      let temp = {
+                        'transaction_id': transactionIdList[i]['id'],
+                        'timestamp': transactionIdList[i]['created_at'],
+                        'amount': transactionIdList[i]['send_amount'],
+                        'status': transactionIdList[i]['status'],
+                        'sender_id': inputInfo[0]['user_id'],
+                        'sender_address': inputInfo[0]['address'],
+                        'ref_hash': inputInfo[0]['ref_hash'],
+                        'ref_index': inputInfo[0]['ref_index'],
+                      };
+                      receiver_data.push(temp);
+                      if (i == transactionIdList.length - 1) {
+                        let data = {
+                          'status': 200,
+                          'report': 'Lấy dữ liệu thành công!',
+                          'data': {
+                            'total_receiver_trans': TotalReceive[0]['total_receive'],
+                            'receiver_trans': receiver_data,
+                            'limit': 10,
+                            'offset': offset
                           }
-                        );
+                        };
+                        res.send(data);
                       }
                     }
                   );
